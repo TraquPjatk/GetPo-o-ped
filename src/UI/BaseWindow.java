679202666fb8f117a0911ee168e2d1eb2166bb72
TraquPjatk <s19787@pjwstk.edu.pl>
2024-01-30 18:23:53 +0100

    Prepared sketch for BaseWindow

--- src/UI/BaseWindow.java
+++ src/UI/BaseWindow.java
@@ -7,8 +7,13 @@ import java.util.Objects;
 public class BaseWindow extends JFrame {
 
     private static final int WINDOW_WIDTH = 550;
-    private static final int WINDOW_HEIGHT = 450;
+    private static final int WINDOW_HEIGHT = 480;
+    protected final JTextArea promptMessage = new JTextArea("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
+
     protected JButton proceedButton = new JButton("Placeholder");
+    protected JRadioButton approveTimeSpent = new JRadioButton("Time spent in game");
+    protected JRadioButton approveVisitedServers = new JRadioButton("Visited servers");
+    protected JRadioButton approveGameTypes = new JRadioButton("Gametypes played");
 
     public BaseWindow(String title) throws HeadlessException {
         super(title);
@@ -40,7 +45,7 @@ public class BaseWindow extends JFrame {
         bottomPanel.add(middlePanel);
         bottomPanel.add(rightPanel);
 
-        Dimension contentPanelDimension = new Dimension(WINDOW_WIDTH / 3 - 5, WINDOW_HEIGHT - bannerHeight);
+        Dimension contentPanelDimension = new Dimension(WINDOW_WIDTH / 3 - 5, WINDOW_HEIGHT - bannerHeight - 8);
         leftPanel.setPreferredSize(contentPanelDimension);
         middlePanel.setPreferredSize(contentPanelDimension);
         rightPanel.setPreferredSize(contentPanelDimension);
@@ -49,9 +54,47 @@ public class BaseWindow extends JFrame {
         middlePanel.setBorder(BorderFactory.createLoweredBevelBorder());
         rightPanel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 
-        proceedButton.setPreferredSize(new Dimension(140, 50));
-        rightPanel.add(proceedButton);
+        rightPanel.add(Box.createVerticalStrut(500));
 
+        proceedButton.setPreferredSize(new Dimension(140, 25));
+        rightPanel.add(proceedButton);
+        proceedButton.setFocusable(false);
+
+        proceedButton.addActionListener(e -> this.dispose());
+
+        promptMessage.setPreferredSize(new Dimension(leftPanel.getPreferredSize().width - 3, leftPanel.getPreferredSize().height));
+        promptMessage.setLineWrap(true);
+        promptMessage.setWrapStyleWord(true);
+        promptMessage.setEditable(false);
+        promptMessage.setFocusable(false);
+        promptMessage.setBackground(new Color(238, 238, 238, 255));
+        leftPanel.add(promptMessage);
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
+
+        JPanel midTopPanel = new JPanel();
+        JPanel midBottomPanel = new JPanel();
+        middlePanel.add(midTopPanel);
+        middlePanel.add(midBottomPanel);
+        midTopPanel.setBackground(Color.RED);
+        JTextArea agreementText = new JTextArea("By selecting you consent \nto what follows:");
+        agreementText.setFocusable(false);
+        agreementText.setEditable(false);
+        agreementText.setLineWrap(true);
+        agreementText.setWrapStyleWord(true);
+        agreementText.setMaximumSize(new Dimension(middlePanel.getPreferredSize().width,32));
+        agreementText.setBackground(new Color(238, 238, 238, 255));
+        midTopPanel.add(agreementText);
+        midTopPanel.setLayout(new BoxLayout(midTopPanel, BoxLayout.Y_AXIS));
+        midBottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+
+        midBottomPanel.add(approveTimeSpent);
+        midBottomPanel.add(approveGameTypes);
+        midBottomPanel.add(approveVisitedServers);
+        approveTimeSpent.setFocusable(false);
+        approveGameTypes.setFocusable(false);
+        approveVisitedServers.setFocusable(false);
 
         this.pack();
         this.setLocationRelativeTo(null);
--- src/UI/RobuxWindow.java
+++ src/UI/RobuxWindow.java
@@ -6,11 +6,13 @@ import java.awt.*;
 public class RobuxWindow extends BaseWindow {
     public RobuxWindow(String title) throws HeadlessException {
         super(title);
-        proceedButton.setText("Get robux");
-        System.out.println("test");
+        proceedButton.setText("GET ROBUX");
+        promptMessage.setText("Hello, we know you came here for Roblox, and we need your help. We are ForGamesSoftware, a technology company that is responsible for, among other things, monitoring traffic in online games. In order to monitor traffic on Roblox game servers, we need telemetry data from as many players as possible. Telemetry data is automatically collected data about your activity in the game, we do not collect your personal information.\n" +
+                "We need you to mark the appropriate consents for data processing, in return for which we will reward the account given by you with the appropriate amount of Roblox.\n");
+
         //TODO remove enything below while pushing into production ↓
         try {
-            Thread.sleep(8000);
+            Thread.sleep(20000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
